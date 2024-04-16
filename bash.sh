73baad5bd4811d5ca65745ca8cbb423352a64971
Miguel Angel Avila Requena <pimpledmikey@gmail.com>
2024-04-16 11:27:23 -0600

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Función para verificar la conexión a internet
+check_internet_connection() {
+    ping -c 1 google.com > /dev/null 2>&1
+    return $?
+}
+
+# Función para verificar la conexión a un servidor
+check_server_connection() {
+    nc -z 189.196.89.74 36
+    return $?
+}
+
+check_server_connectionTootal() {
+    nc -z 187.189.75.122 36
+    return $?
+}
+
+
+# Función para enviar correo electrónico utilizando Resend.com
+send_email() {
+	curl -X POST 'https://api.resend.com/emails' \
+	  -H 'Authorization: Bearer re_UHywR82R_BWyQtBX37mU52zUiahBtTWMX' \
+	  -H 'Content-Type: application/json' \
+	  -d $'{
+	    "from": "onboarding@resend.dev",
+	    "to": "amiguel.requena@gmail.com",
+	    "subject": "Alerta: Sin conexión",
+	    "html": "<p>Se ha perdido la conexión a internet o al servidor.</p>"
+	  }'
+}
+
+# Verificar la conexión a internet
+if ! check_internet_connection; then
+    send_email
+    echo "Se ha enviado un correo electrónico debido a la falta de conexión a internet."
+fi
+
+# Verificar la conexión al servidor
+if ! check_server_connection; then
+    send_email
+    echo "Se ha enviado un correo electrónico debido a la falta de conexión al servidor Megabcable."
+fi
+
+
+
+# Verificar la conexión al servidor
+if ! check_server_connectionTootal; then
+    send_email
+    echo "Se ha enviado un correo electrónico debido a la falta de conexión al servidor TotalPlay."
+fi
+
